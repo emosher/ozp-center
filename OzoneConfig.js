888c3b626c2bac77511fd19ee98db4ecd91fc04d
Nish <nishantpatel611@gmail.com>
2015-02-04 11:12:43 -0500

    Update 2015-02-04T16:12:43.808Z

---
+++ OzoneConfig.js
@@ -0,0 +1,9 @@
+window.OzoneConfig = {
+    "API_URL": 'https://www.owfgoss.org/ng/dev/mp',
+    "HELP_URL": "https://localhost:8088/dist/assets/PlaceholderUserGuide.pdf",
+    "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics",
+    "CENTER_URL": "http://localhost:8000/dist",
+    "HUD_URL": 'http://ozone-development.github.io/hud-ui/',
+    "WEBTOP_URL": "http://localhost:9000/#/grid/sticky-0/0",
+    "DEVELOPER_RESOURCES_URL": "#",
+};
