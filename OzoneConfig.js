04f0dcb3522aef90b30ab2c13a2ebaf3aaf5c652
Greg Ziskind <gregory.ziskind@synergistcomputing.com>
2015-02-11 17:05:50 -0500

    Update 2015-02-11T22:05:50.123Z

--- OzoneConfig.js
+++ OzoneConfig.js
@@ -1,7 +1,7 @@
 window.OzoneConfig = {
     "API_URL": 'https://www.owfgoss.org/ng/dev/mp',
     "HELP_URL": 'http://ozone-development.github.io/hud-ui/assets/PlaceholderUserGuide.pdf',
-    "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics",
+    "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics/",
     "CENTER_URL": "http://localhost:8000/dist",
     "HUD_URL": 'http://ozone-development.github.io/hud-ui/',
     "WEBTOP_URL": 'http://ozone-development.github.io/ozp-webtop/#/grid/sticky-0/0',
